03cef0bb8f1771fea26dd0ce8467d2730b45dd7d
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-08 00:26:06 -0400

    Update index.js
    updated cors methods

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose
 app.use(
   cors({
     origin: "https://stupendous-selkie-e5aa00.netlify.app", // Replace with the actual origin of your frontend
-    methods: ["GET", "POST", "DELETE", "PUT"], // Include the HTTP methods you need
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"], // Include the HTTP methods you need
   })
 );
 
