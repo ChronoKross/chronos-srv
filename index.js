ea9d0dd7174c7a3f12e3332557ca16d71ff13f3f
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-09 23:12:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Define a catch-all route that serves your main HTML file
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
