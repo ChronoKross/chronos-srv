036d4dfc6ddb7b67bdb9eb7754c7822990295a32
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-09 21:49:52 -0400

    Update Auth.js
    changed post to a get request for login route

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const Admin = require("../models/Admin");
-// Library to hash password to keep it private even if the database is breached
 const bcrypt = require("bcrypt");
 
 // REGISTER
 router.post("/register", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(15); // 10 is default, increasing password security if >
+    const salt = await bcrypt.genSalt(10); // 10 is default, increasing password security if >
     const hashPass = await bcrypt.hash(req.body.password, salt);
     const newAdmin = new Admin({
       userName: req.body.username,
@@ -21,7 +20,7 @@ router.post("/register", async (req, res) => {
 });
 
 // LOGIN
-router.post("/login", async (req, res) => {
+router.get("/login", async (req, res) => {
   try {
     const admin = await Admin.findOne({ userName: req.body.username });
     if (!admin) {
