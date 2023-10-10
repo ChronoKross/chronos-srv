40e3098f4f8bc1280a62541ebaf27e5d29a96834
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-09 22:54:11 -0400

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema(
   {
-    username: {
+    userName: {
       type: String,
       required: true,
       unique: true, // Define it as unique to enforce uniqueness
