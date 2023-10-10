caed2ea552bfda2f0cc24d7a3f92e02464ffab81
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-09 21:39:40 -0400

    Update Auth.js

--- routes/Auth.js
+++ routes/Auth.js
@@ -23,7 +23,7 @@ router.post("/register", async (req, res) => {
 // LOGIN
 router.post("/login", async (req, res) => {
   try {
-    const admin = await Admin.findOne({ userName: req.body.userName });
+    const admin = await Admin.findOne({ userName: req.body.username });
     if (!admin) {
       return res.status(400).json("Wrong credentials.");
     }
