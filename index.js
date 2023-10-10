16a13d7987a5912f932eee61b6d138857b6aebe3
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-09 23:07:20 -0400

    routes
    try fix routes wont refresh

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the provided port or default to 3000
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const employeeRoute = require("./routes/employees");
 const adminRoute = require("./routes/Admin");
 const cors = require("cors");
 const adminAuth = require("./routes/Auth");
-//APP CURRENTLY WORKING
+const path = require("path"); // Import the "path" module
 
 dotenv.config();
 app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(console.log("Connected to MONGODB"))
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(
   cors({
@@ -28,6 +32,14 @@ app.use("/employee", employeeRoute);
 app.use("/admin", adminRoute);
 app.use("/auth", adminAuth);
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Define a catch-all route that serves your main HTML file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
