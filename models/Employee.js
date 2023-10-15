4aba8ad7e3de0319ac4a4b74ff8a8253b8e23ea2
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-14 22:33:19 -0400

    updated Employee MODEL
    seeded db with new timeOff field which is an array of objects

--- models/Employee.js
+++ models/Employee.js
@@ -23,9 +23,15 @@ const employeeSchema = new mongoose.Schema(
 );
 
 employeeSchema.methods.calculateTotalTimeOff = function () {
+  const millisecondsInAnHour = 60 * 60 * 1000; // 60 minutes in an hour
+  const currentDate = new Date();
+
   return this.timeOff.reduce((totalTimeOff, entry) => {
-    const timeOffDuration = entry.date.getTime();
-    totalTimeOff += timeOffDuration;
+    const timeOffStart = entry.date;
+    const timeOffEnd = currentDate; // Assuming you are calculating up to the current date
+    const timeOffDurationInHours =
+      (timeOffEnd - timeOffStart) / millisecondsInAnHour;
+    totalTimeOff += timeOffDurationInHours;
 
     return totalTimeOff;
   }, 0);
--- utils/seedDB.js
+++ utils/seedDB.js
@@ -1,28 +1,28 @@
-// const mongoose = require("mongoose");
-// const Employee = require("../models/employee");
-// require("dotenv").config();
+const mongoose = require("mongoose");
+const Employee = require("../models/Employee");
+require("dotenv").config();
 
-// mongoose
-//   .connect(process.env.MONGO_URL)
-//   .then(() => console.log("Connected to MONGODB"))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MONGODB"))
+  .catch((err) => console.log(err));
 
-// const seedData = [
-//   { name: "Nathan", position: 1 },
-//   { name: "Eric", position: 2 },
-//   { name: "John", position: 3 },
-//   { name: "Doug", position: 4 },
-//   { name: "Johny", position: 5 },
-//   { name: "Red", position: 6 },
-//   { name: "Jose", position: 7 },
-//   { name: "Ivan", position: 8 },
-// ];
+const seedData = [
+  { name: "Nathan", position: 1, timeOff: [{ date: test, type: volunteer }] },
+  { name: "Eric", position: 2, timeOff: [{ date: test, type: volunteer }] },
+  { name: "John", position: 3, timeOff: [{ date: test, type: volunteer }] },
+  { name: "Doug", position: 4, timeOff: [{ date: test, type: volunteer }] },
+  { name: "Johny", position: 5, timeOff: [{ date: test, type: volunteer }] },
+  { name: "Red", position: 6, timeOff: [{ date: test, type: volunteer }] },
+  { name: "Jose", position: 7, timeOff: [{ date: test, type: volunteer }] },
+  { name: "Ivan", position: 8, timeOff: [{ date: test, type: volunteer }] },
+];
 
-// const seedDB = async () => {
-//   await Employee.deleteMany({});
-//   await Employee.insertMany(seedData);
-// };
+const seedDB = async () => {
+  await Employee.deleteMany({});
+  await Employee.insertMany(seedData);
+};
 
-// seedDB().then(() => {
-//   mongoose.connection.close();
-// });
+seedDB().then(() => {
+  mongoose.connection.close();
+});
