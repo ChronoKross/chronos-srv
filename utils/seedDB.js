127aec2a5c31de52364961fe44b83fb5d0fb8246
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-14 22:52:42 -0400

    Update seedDB.js

--- utils/seedDB.js
+++ utils/seedDB.js
@@ -13,17 +13,41 @@ const seedData = [
     position: 1,
     timeOff: [{ date: "test", type: "volunteer" }],
   },
-  { name: "Eric", position: 2, timeOff: [{ date: "test", type: "volunteer" }] },
-  { name: "John", position: 3, timeOff: [{ date: "test", type: "volunteer" }] },
-  { name: "Doug", position: 4, timeOff: [{ date: "test", type: "volunteer" }] },
+  {
+    name: "Eric",
+    position: 2,
+    timeOff: [{ date: Date.now(), type: "volunteer" }],
+  },
+  {
+    name: "John",
+    position: 3,
+    timeOff: [{ date: Date.now(), type: "volunteer" }],
+  },
+  {
+    name: "Doug",
+    position: 4,
+    timeOff: [{ date: Date.now(), type: "volunteer" }],
+  },
   {
     name: "Johny",
     position: 5,
-    timeOff: [{ date: "test", type: "volunteer" }],
+    timeOff: [{ date: Date.now(), type: "volunteer" }],
   },
-  { name: "Red", position: 6, timeOff: [{ date: "test", type: "volunteer" }] },
-  { name: "Jose", position: 7, timeOff: [{ date: "test", type: "volunteer" }] },
-  { name: "Ivan", position: 8, timeOff: [{ date: "test", type: "volunteer" }] },
+  {
+    name: "Red",
+    position: 6,
+    timeOff: [{ date: Date.now(), type: "volunteer" }],
+  },
+  {
+    name: "Jose",
+    position: 7,
+    timeOff: [{ date: Date.now(), type: "volunteer" }],
+  },
+  {
+    name: "Ivan",
+    position: 8,
+    timeOff: [{ date: Date.now(), type: "volunteer" }],
+  },
 ];
 
 const seedDB = async () => {
