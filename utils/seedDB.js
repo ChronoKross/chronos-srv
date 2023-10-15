5283e394f04002264f30dcaa5accb29c695de986
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-14 22:56:00 -0400

    Update seedDB.js

--- utils/seedDB.js
+++ utils/seedDB.js
@@ -11,37 +11,37 @@ const seedData = [
   {
     name: "Nathan",
     position: 1,
-    timeOff: [{ date: "test", type: "volunteer" }],
+    timeOff: [{ date: new Date.now(), type: "volunteer" }],
   },
   {
     name: "Eric",
     position: 2,
-    timeOff: [{ date: Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date.now(), type: "volunteer" }],
   },
   {
     name: "John",
     position: 3,
-    timeOff: [{ date: Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date.now(), type: "volunteer" }],
   },
   {
     name: "Doug",
     position: 4,
-    timeOff: [{ date: Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date.now(), type: "volunteer" }],
   },
   {
     name: "Johny",
     position: 5,
-    timeOff: [{ date: Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date.now(), type: "volunteer" }],
   },
   {
     name: "Red",
     position: 6,
-    timeOff: [{ date: Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date.now(), type: "volunteer" }],
   },
   {
     name: "Jose",
     position: 7,
-    timeOff: [{ date: Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date.now(), type: "volunteer" }],
   },
   {
     name: "Ivan",
