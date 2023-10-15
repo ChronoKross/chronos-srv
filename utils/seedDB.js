d40d8eb963b2719c3a2a15d9b0d7cd57cc93fe60
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-14 23:03:53 -0400

    Update seedDB.js

--- utils/seedDB.js
+++ utils/seedDB.js
@@ -3,58 +3,66 @@ const Employee = require("../models/Employee");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("Connected to MONGODB"))
-  .catch((err) => console.log(err));
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 const seedData = [
   {
     name: "Nathan",
     position: 1,
-    timeOff: [{ date: new Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date(), type: "volunteer" }],
   },
   {
     name: "Eric",
     position: 2,
-    timeOff: [{ date: new Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date(), type: "volunteer" }],
   },
   {
     name: "John",
     position: 3,
-    timeOff: [{ date: new Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date(), type: "volunteer" }],
   },
   {
     name: "Doug",
     position: 4,
-    timeOff: [{ date: new Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date(), type: "volunteer" }],
   },
   {
     name: "Johny",
     position: 5,
-    timeOff: [{ date: new Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date(), type: "volunteer" }],
   },
   {
     name: "Red",
     position: 6,
-    timeOff: [{ date: new Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date(), type: "volunteer" }],
   },
   {
     name: "Jose",
     position: 7,
-    timeOff: [{ date: new Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date(), type: "volunteer" }],
   },
   {
     name: "Ivan",
     position: 8,
-    timeOff: [{ date: new Date.now(), type: "volunteer" }],
+    timeOff: [{ date: new Date(), type: "volunteer" }],
   },
 ];
 
 const seedDB = async () => {
-  await Employee.deleteMany({});
-  await Employee.insertMany(seedData);
+  try {
+    await Employee.deleteMany({});
+    await Employee.insertMany(seedData);
+    console.log("Data seeded successfully.");
+  } catch (error) {
+    console.error("Error seeding data:", error);
+  } finally {
+    mongoose.connection.close();
+  }
 };
 
-seedDB().then(() => {
-  mongoose.connection.close();
-});
+seedDB();
